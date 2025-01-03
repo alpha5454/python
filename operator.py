82a17b2cbc7baf12d923cb37015d7981ac323f26
Karthick V <karthickvelu5454@gmail.com>
2025-01-03 11:07:37 +0530

    python
    python basics,operators,ifelse...,

---
+++ operator.py
@@ -0,0 +1,35 @@
+a = 25
+b = 10
+# addition
+print("Addition", a+b)
+# subraction
+print("Subraction", a-b)
+# multiplication
+print("Multiplication", a*b)
+# division
+print("Division", a/b)
+# a greater than b
+print ("greater than",a>b)
+# a less than b
+print("less than",a<b)
+# a and b are equal
+print("equal to",a==b)
+
+# additon
+add = a+b
+# subraction
+sub = a-b
+# multiplication
+mul= a*b
+#division
+div=a/b
+# greater than
+gre = a>b
+# less than 
+less=a<b
+# equal to
+equal = a==b
+
+
+
+
